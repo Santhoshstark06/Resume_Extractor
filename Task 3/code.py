ed6b8955b7e978cede76ac815393d9687db81845
A. Santhosh Kumar <58947968+Santhoshstark06@users.noreply.github.com>
2023-09-19 14:07:02 +0530

    Update code.py

--- Task 3/code.py
+++ Task 3/code.py
@@ -1,32 +1,38 @@
-pip install transformers numpy
-import numpy as np
+import torch
 from transformers import DistilBertTokenizer, DistilBertModel
 from sklearn.metrics.pairwise import cosine_similarity
-model_name = 'distilbert-base-uncased'
-tokenizer = DistilBertTokenizer.from_pretrained(model_name)
-model = DistilBertModel.from_pretrained(model_name)
-def get_embeddings(text, tokenizer, model):
-    tokens = tokenizer(text, padding=True, truncation=True, return_tensors='pt')
-    with torch.no_grad():
-        outputs = model(**tokens)
-    return outputs.last_hidden_state.mean(dim=1).numpy()
-def calculate_similarity(job_description_embedding, cv_embeddings):
-    similarities = cosine_similarity(job_description_embedding, cv_embeddings)
-    return similarities
-def rank_cvs(job_description, cv_details, tokenizer, model):
-    job_description_embedding = get_embeddings(job_description, tokenizer, model)
-    cv_embeddings = get_embeddings(cv_details, tokenizer, model)
-    similarities = calculate_similarity(job_description_embedding, cv_embeddings)
-    ranking = np.argsort(similarities[0])[::-1]  # Sort in descending order
-    top_5_cvs = ranking[:5]
-    return top_5_cvs
-job_descriptions = [...]  
-cv_details = [...]  
+
+
+job_descriptions = ["Job description 1 text here.", "Job description 2 text here.", ...] 
+cvs = ["CV 1 text here.", "CV 2 text here.", ...] 
+
+
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+model = DistilBertModel.from_pretrained('distilbert-base-uncased')
+
+job_description_embeddings = []
+cv_embeddings = []
 
 for job_description in job_descriptions:
-    top_5_cvs = rank_cvs(job_description, cv_details, tokenizer, model)
-    print("Job Description:", job_description)
-    print("Top 5 Matching CVs:")
-    for idx in top_5_cvs:
-        print(cv_details[idx])
-    print("\n")
+    tokens = tokenizer(job_description, return_tensors='pt', padding=True, truncation=True)
+    with torch.no_grad():
+        output = model(**tokens)
+    job_description_embeddings.append(output.last_hidden_state.mean(dim=1).numpy())
+
+for cv in cvs:
+    tokens = tokenizer(cv, return_tensors='pt', padding=True, truncation=True)
+    with torch.no_grad():
+        output = model(**tokens)
+    cv_embeddings.append(output.last_hidden_state.mean(dim=1).numpy())
+
+
+similarity_scores = cosine_similarity(job_description_embeddings, cv_embeddings)
+
+
+top_5_cvs_per_job_description = []
+
+for i, job_description in enumerate(job_descriptions):
+    cv_scores = list(enumerate(similarity_scores[i]))
+    sorted_cvs = sorted(cv_scores, key=lambda x: x[1], reverse=True)[:5]
+    top_5_cvs = [(cvs[idx], score) for idx, score in sorted_cvs]
+    top_5_cvs_per_job_description.append((job_description, top_5_cvs))
